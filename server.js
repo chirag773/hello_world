35390c90f45fe6961b1b1bee8f84bcd0571a2f73
chirag <chiragprajapati781@gmail.com>
2018-07-06 12:54:05 +0530

    new

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express           = require('express');
 var app               = express();
 var bodyParser        = require("body-parser");
+
 var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
 var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
